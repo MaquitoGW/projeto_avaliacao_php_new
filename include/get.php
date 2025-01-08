a4d4905b1f6cc12737f9a612e41da7bf6852da58
MaquitoGw <maicongoncalves826@gmail.com>
2025-01-08 16:38:18 -0300

    added ascending sorting of the table

--- include/get.php
+++ include/get.php
@@ -2,7 +2,7 @@
 $response = new Response();
 
 // Buscar funcionarios
-$query = $pdo->prepare("SELECT f.id_funcionario, f.nome, f.cpf, f.rg, f.email, e.nome as empresa FROM tbl_funcionario f JOIN tbl_empresa e ON f.id_empresa = e.id_empresa");
+$query = $pdo->prepare("SELECT f.id_funcionario, f.nome, f.cpf, f.rg, f.email, f.data_cadastro, f.salario, f.bonificacao,  e.nome as empresa FROM tbl_funcionario f JOIN tbl_empresa e ON f.id_empresa = e.id_empresa ORDER BY f.id_funcionario ASC");
 $query->execute();
 $funcionarios = $query->fetchAll();
 
